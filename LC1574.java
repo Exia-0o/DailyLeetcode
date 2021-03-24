a2d70588f61e56f7e8b4837251be822e8830a912
XingHang Li <70033686+Exia-0o@users.noreply.github.com>
2021-03-23 23:40:59 -0700

    Create LC1574.java

---
+++ LC1574.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int findLengthOfShortestSubarray(int[] nums) {
+        int start = 0;
+        int end = nums.length - 1;
+        while (start < end && nums[start] <= nums[start + 1])
+            start++;
+        if (start == end)
+            return 0;
+        while(start <= end && nums[end] >= nums[end - 1])
+            end--;
+        if (end == 0)
+            return nums.length - 1;
+        
+        int ans = Math.min(end, nums.length - 1 - start);
+        
+        int i = 0;
+        int j = end;
+        
+        while (i <= start && j < nums.length){
+            if (nums[j] >= nums[i]){
+                ans = Math.min(j - i - 1, ans);
+                i++;
+            }
+            else
+                j++;
+        }
+        return ans;
+   
