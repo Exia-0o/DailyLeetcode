90f6d47153f113c307698e8aa6540fdf1ade0920
XingHang Li <70033686+Exia-0o@users.noreply.github.com>
2021-03-29 19:56:59 -0700

    Create LC410.java

---
+++ LC410.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int splitArray(int[] nums, int m) {
+        int n = nums.length;
+        int[][] dp = new int[n + 1][m + 1];
+        int[] prefixSum = new int[n + 1];
+        for (int i = 0; i < n; i++) {
+            prefixSum[i + 1] = prefixSum[i] + nums[i];
+        }
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= m; j++) {
+                dp[i][j] = Integer.MAX_VALUE;
+            }
+        }
+        dp[0][0] = 0;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                for (int k = 0; k < i; k++) {
+                    dp[i][j] = Math.min(dp[i][j], Math.max(dp[k][j - 1], prefixSum[i] - prefixSum[k]));
+                }
+            }
+        }
+        return dp[n][m];        
+    }
+}
